8d573885c63dc2cd7ff47a9f5d1f64353037b28f
SlavkaVavrinova <slavka.vav@gmail.com>
2021-12-08 01:30:10 +0100

    menu

--- menu.js
+++ menu.js
@@ -2,7 +2,15 @@
 
 const menuOpenElm = document.querySelector('#menu__button');
 const menu = document.querySelector('#menu__open');
+const cross = document.querySelector('#cross');
+const contactElm = document.querySelector('#contact-nav');
 
 menuOpenElm.addEventListener('click', () => {
   menu.classList.add('menu-opened');
+  contactElm.classList.add('contact-menu-opened');
+});
+
+cross.addEventListener('click', () => {
+  menu.classList.remove('menu-opened');
+  contactElm.classList.remove('contact-menu-opened');
 });
