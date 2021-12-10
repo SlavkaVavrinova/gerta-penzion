32dde9ff4e6497bd6edf0537090e55943a8f1a19
SlavkaVavrinova <slavka.vav@gmail.com>
2021-12-10 13:46:26 +0100

    nav, covid, buttons

--- menu.js
+++ menu.js
@@ -3,6 +3,7 @@
 const menuOpenElm = document.querySelector('#menu__button');
 const menu = document.querySelector('#menu__open');
 const cross = document.querySelector('#cross');
+
 const contactElm = document.querySelector('#contact');
 const roomsElm = document.querySelector('#rooms');
 
@@ -16,4 +17,15 @@ cross.addEventListener('click', () => {
   menu.classList.remove('menu-opened');
   contactElm.classList.remove('contact-menu-opened');
   roomsElm.classList.remove('rooms-menu-opened');
+});
+
+const languageElm = document.querySelector('#language');
+const languageSelectElm = document.querySelector('#language__select');
+
+languageElm.addEventListener('click', () => {
+  languageSelectElm.classList.add('language-select--display');
+});
+
+languageSelectElm.addEventListener('click', () => {
+  languageSelectElm.classList.remove('language-select--display');
 });
